ae0e3d6846102265e8c956c64b3a182afef0dc46
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-06-01 14:08:20 +0600

    add reviewa

--- src/pages/home/reviews/ReviewCard.jsx
+++ src/pages/home/reviews/ReviewCard.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import { FaHeart } from "react-icons/fa";
 const ReviewCard = () => {
     return (
-        <div>
-            
+        <div className='border rounded-md p-4'>
+            <div className='flex justify-between'>
+                <div className='flex gap-4 items-center'>
+                    <div className="avatar">
+                        <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                            <img src="https://i.ibb.co/wgstTQ2/unnamed.png" />
+                        </div>
+                    </div>
+                    <div>
+                        <h2 className='font-semibold text-xl'>Ashaduzaman</h2>
+                        <h3>React Developer</h3>
+                    </div>
+                </div>
+                <div>
+                    <span><FaHeart className='text-5xl text-orange-500'></FaHeart></span>
+                </div>
+            </div>
+            <div className='mt-2'>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quasi voluptatem corporis et provident tempora perferendis suscipit eos culpa laudantium, voluptas repellendus animi vel eveniet, quam soluta obcaecati commodi dignissimos?</p>
+            </div>
         </div>
     );
 };
--- src/pages/home/reviews/Reviews.jsx
+++ src/pages/home/reviews/Reviews.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import SectionTitle from '../../../components/sectionTitle/SectionTitle';
+import ReviewCard from './ReviewCard';
 const Reviews = () => {
     return (
         <div>
             <SectionTitle heading="What Your Patient Says" subHeading="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta numquam praesentium assumenda provident, exercitationem qui alias. Totam quas eum laboriosam?"></SectionTitle>
+            <div className='flex justify-between items-center gap-6'>
+                <ReviewCard></ReviewCard>
+                <ReviewCard></ReviewCard>
+            </div>
         </div>
     );
 };
