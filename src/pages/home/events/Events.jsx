2c313a4f7ed2a1ae4fae4ad4241357e6a621bdd9
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-06-01 14:11:38 +0600

    add event

--- src/pages/home/events/Events.jsx
+++ src/pages/home/events/Events.jsx
@@ -3,7 +3,7 @@ import EventCard from './EventCard';
 
 const Events = () => {
     return (
-        <div className='grid grid-cols-3 gap-6 mb-4'>
+        <div className='grid grid-cols-3 gap-6 py-6'>
             <EventCard></EventCard>
             <EventCard></EventCard>
             <EventCard></EventCard>
